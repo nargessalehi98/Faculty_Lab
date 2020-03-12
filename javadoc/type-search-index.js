d7804004b6040fdf8a81feecf1404058c3d56757
nargessalehi98 <47316389+nargessalehi98@users.noreply.github.com>
2020-03-12 16:16:20 +0330

    javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Course"},{"p":"<Unnamed>","l":"Faculty"},{"p":"<Unnamed>","l":"Lab"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Professor"},{"p":"<Unnamed>","l":"Student"}]
